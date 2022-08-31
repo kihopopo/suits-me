637020ffd0a86e66f06fb80d91e9c973feff2847
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-08-31 15:02:12 +0000

    update commit8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :genres
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
