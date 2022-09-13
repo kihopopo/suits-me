37f551c25327bb4a85788bf5b2a50c4f2983a77b
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-09-13 09:46:39 +0000

    update commit18

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
-  
-  enum is_active: {"販売中": true,"販売停止中": false}
-    
+
   def add_tax_price
     (self.price*1.10).round
   end
-    
+
   def with_tax_price
-    (price * 1.1).floor
+    (price * 1.1).ceil
+    # ceilは切り上げ、floorが切り捨て、roundが四捨五入
   end
-    
+
   def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     image.variant(resize_to_limit: [width, height]).processed
