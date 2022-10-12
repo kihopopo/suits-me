af70c78392b966355ef6af626a22531b818e189d
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-10-12 13:45:11 +0000

    [fix]アクセス制限、製作ステータスのエラー修正
    注文履歴一覧のviewにturbolinksを記載、製作ステータスが更新されないエラー修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,5 +10,9 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting: 0, paid_up: 1, production: 2, preparing: 3, shipped: 4}
 
+  def are_all_details_completed?
+    (order_details.completed.count == order_details.count) ? true : false
+  end
+
 end
 
