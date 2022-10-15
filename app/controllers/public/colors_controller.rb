6395789ed0a210366d43687e4f4ac2cb2d4ea7a8
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-10-15 11:00:19 +0000

    [fix]注文情報入力画面(ECサイト)のラジオボタン、[improve]診断機能のバリデーション
    支払方法が空欄になるのを防ぐためクレジットカードにチェックが入っている状態にした、診断機能でちゃんとチェックしないと診断不可能とフラッ シュメッセージが出るようにした

--- app/controllers/public/colors_controller.rb
+++ app/controllers/public/colors_controller.rb
@@ -6,11 +6,21 @@ class Public::ColorsController < ApplicationController
   def create
     @color = Color.new(color_params)
     params[:color][:question] ? @color.question = params[:color][:question].join("") : false
-    if @color.save
+    #TODO: 論理演算子に関する補足: https://pikawaka.com/ruby/logical-operators
+    #Array#include?を用いた
+    valid = false
+    if @color.question.include?("1") && @color.question.include?("2")
+    elsif @color.question.include?("3") && @color.question.include?("4")
+    elsif @color.question.include?("5") && @color.question.include?("6")
+    elsif @color.question.include?("7") && @color.question.include?("8")
+    else
+      valid = true
+    end
+    if valid && @color.question.size == 4 && @color.save
         flash[:success] = "診断が完了しました"
         redirect_to color_path(@color.id)
     else
-        flash[:danger] = "診断ができませんでした。もう一度ご確認ください"
+        flash[:danger] = "診断ができませんでした。もう一度診断し直してください。"
         render :index
     end
   end
