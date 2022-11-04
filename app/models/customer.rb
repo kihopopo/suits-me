f4c4d92b87f2956ea73d7c74341554446e567798
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-11-04 06:56:55 +0000

    [fix]文字数制限が分かるように表示
    新規登録(Sign up)のフォームに文字数制限(username:ユーザーネーム)があるにもかかわらず、ECサイトに文字制限がわかる記載がなく、修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,7 +17,7 @@ class Customer < ApplicationRecord
  validates :last_name, presence:true
  validates :first_name_kana, presence:true
  validates :last_name_kana, presence:true
- validates :username, presence:true, length: { maximum: 20 }
+ validates :username, presence:true, length: { maximum: 15 }
  validates :postal_code, presence:true
  validates :address, presence:true
  validates :telephone_number, presence:true
