07493080d9d918d2e1466c5f8e86b1fce7a6ac93
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-09 08:59:41 +0000

    [update]jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 window.$ = window.jQuery = require('jquery');
 require('packs/raty')
+require('jquery')
 
 Rails.start()
 Turbolinks.start()
