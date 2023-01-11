8ad5f1364ff7462c35458579e98c539dce3138b3
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-11 09:46:37 +0000

    [update]非同期通信(いいね機能)

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,27 @@
 class Public::FavoritesController < ApplicationController
+  before_action :post_image_params
 
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_customer.favorites.new(post_image_id: post_image.id)
-    favorite.save
+    Favorite.create(customer_id: current_customer.id, post_image_id: params[:id])
+    # # post_image = PostImage.find(params[:post_image_id])
+    # favorite = current_customer.favorites.new(post_image_id: post_image.id)
+    # favorite.save
     # redirect_to post_image_path(post_image)
   end
 
   def destroy
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_customer.favorites.find_by(post_image_id: post_image.id)
-    favorite.destroy
+    Favorite.create(customer_id: current_customer.id, post_image_id: params[:id]).destroy
+    # post_image = PostImage.find(params[:post_image_id])
+    # favorite = current_customer.favorites.find_by(post_image_id: post_image.id)
+    # favorite.destroy
     # redirect_to post_image_path(post_image)
   end
 
+
+  private
+
+  def post_image_params
+    @post_image = PostImage.find(params[:id])
+  end
 end
+
